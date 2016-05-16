080e88fa97ae6e195e13c1a365cc6b7ac824191e
Shaun Yap <shaun@siegeflow.com>
2016-05-16 14:25:27 +0400

    created basic frontend

--- server.js
+++ server.js
@@ -8,16 +8,26 @@ var moment = require('moment');
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views')
 
-app.get('/',  function(req, res) {
-    tide().then(function (body) {
-            tides = body.extremes;
+app.get('/', function(req, res) {
+    tide().then(function(body) {
+        tides = body.extremes;
+        tideDate = new Date(tides[0].dt * 1000);
+        tideDay = tideDate.getDate();
+        monthNames = ["January", "February", "March", "April",
+            "May", "June", "July", "August", "September",
+            "October", "November", "December"
+        ];
+        tideMonth = monthNames[tideDate.getMonth()];
+        dateString = tideDay + ' ' + tideMonth;
     }).then(function() {
-        res.render('main');
- });
+        res.render('main', {
+            date: dateString
+        });
+    });
 });
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(PORT, function () {
-  console.log('starting server at port ' + PORT +'..');
+app.listen(PORT, function() {
+    console.log('starting server at port ' + PORT + '..');
 });
